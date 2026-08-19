0a4d05d09abbe90c0baad502200502d0c0b9ca2f
sonusingh789 <itsmesonu47@gmail.com>
2026-08-19 20:51:20 +0530

    commit

--- Star&NumberPatterns/Pattern.java
+++ Star&NumberPatterns/Pattern.java
@@ -27,6 +27,98 @@ public class Pattern {
 
     // -----------------------------------------
 
+    static void pattern2() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p2j(int n, int count) {
+        if (count >= n) {
+            return;
+        }
+        System.out.print("*");
+        p2j(n, count + 1);
+    }
+
+    static void p2i(int n) {
+        if (n > 4) {
+            return;
+        }
+        p2j(n, 0);
+        System.out.println(" ");
+        p2i(n + 1);
+    }
+
+    // ---------------------------------------------
+
+    static void pattern3() {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    static void p3j(int n, int count) {
+        if (count > n) {
+            return;
+        }
+        System.out.print(count);
+        p3j(n, count + 1);
+    }
+
+    static void p3i(int n) {
+        if (n > 6) {
+            return;
+        }
+        p3j(n, 1);
+        System.out.println(" ");
+        p3i(n + 1);
+    }
+
+    // -----------------------------------------------
+    static void pattern4() {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    static void p4j(int n, int count) {
+        if (count > n) {
+            return;
+        }
+        System.out.print(n);
+        p4j(n, count + 1);
+    }
+
+    static void p4i(int n) {
+        if (n > 6) {
+            return;
+        }
+        p4j(n, 1);
+        System.out.print("");
+        p4i(n + 1);
+    }
+
+    // -----------------------------------------------
+    static void pattern5() {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 0; j < 6 - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // --------------------------------------------------
     public static void main(String[] args) {
         // * * * *
         // * * * *
@@ -37,5 +129,35 @@ public class Pattern {
 
         // ----------------------------------
 
+        // *
+        // * *
+        // * * *
+        // * * * *
+
+        // pattern2(); iterative
+        // p2i(0); recursive
+
+        // ---------------------------------
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+        // pattern3(); iterative
+        // p3i(1); recursive
+
+        // ----------------------------------------
+        // 1
+        // 2 2
+        // 3 3 3
+        // 4 4 4 4
+        // 5 5 5 5 5
+        // pattern4(); iterative
+        // p4i(1); recursive
+
+        // ---------------------------------------------
+        // pattern5();
+
     }
+
 }
