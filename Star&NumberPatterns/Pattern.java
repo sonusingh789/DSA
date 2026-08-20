45295abb45cd17c33771811022078a4967eee5e8
sonusingh789 <itsmesonu47@gmail.com>
2026-08-20 17:54:52 +0530

    commit

--- Star&NumberPatterns/Pattern.java
+++ Star&NumberPatterns/Pattern.java
@@ -118,7 +118,59 @@ public class Pattern {
         }
     }
 
+    static void p5j(int n, int count) {
+        if (count > n) {
+            return;
+        }
+
+        System.out.print("*" + " ");
+        p5j(n, count + 1);
+
+    }
+
+    static void p5i(int n) {
+        if (n > 5) {
+            return;
+        }
+        p5j(4 - n, 0);
+        System.out.println("");
+        p5i(n + 1);
+    }
+
     // --------------------------------------------------
+
+    static void pattern6() {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6 - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p6j(int n, int count) {
+        if (count > n) {
+            return;
+        }
+
+        System.out.print(count + " ");
+        p6j(n, count + 1);
+    }
+
+    static void p6i(int n) {
+        if (n > 6) {
+            return;
+        }
+        p6j(6 - n, 1);
+        System.out.println(" ");
+        p6i(n + 1);
+    }
+
+    // --------------------------------------------
+    static void pattern7() {
+
+    }
+
     public static void main(String[] args) {
         // * * * *
         // * * * *
@@ -156,8 +208,21 @@ public class Pattern {
         // p4i(1); recursive
 
         // ---------------------------------------------
+        // * * * * *
+        // * * * *
+        // * * *
+        // * *
+        // *
         // pattern5();
-
+        // p5i(0);
+        // ------------------------------------------------
+        // 1 2 3 4 5
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+        // pattern6(); iterative
+        // p6i(1); recursive
     }
 
 }
